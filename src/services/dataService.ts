9b30581ed551918f0a6fa9d2a539d53bd512789d
Tran Duc Loc <tranducloc2010@gmail.com>
2025-08-11 14:49:02 +0700

    Update search and paging in page category

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,20 +1,24 @@
 import axiosClient from "./axiosClient";
 
 class DataService {
-  get<T>(url: string, params?: any) {
-    return axiosClient.get<T>(url, { params }).then(res => res.data);
+  async get<T>(url: string, params?: any): Promise<T> {
+    const response = await axiosClient.get<T>(url, { params });
+    return response.data;
   }
 
-  post<T>(url: string, data: any) {
-    return axiosClient.post<T>(url, data).then(res => res.data);
+  async post<T>(url: string, data: any): Promise<T> {
+    const response = await axiosClient.post<T>(url, data);
+    return response.data;
   }
 
-  put<T>(url: string, data: any) {
-    return axiosClient.put<T>(url, data).then(res => res.data);
+  async put<T>(url: string, data: any): Promise<T> {
+    const response = await axiosClient.put<T>(url, data);
+    return response.data;
   }
 
-  delete<T>(url: string) {
-    return axiosClient.delete<T>(url).then(res => res.data);
+  async delete<T>(url: string): Promise<T> {
+    const response = await axiosClient.delete<T>(url);
+    return response.data;
   }
 }
 
